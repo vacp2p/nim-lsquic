909968505a69d168f55163842f2d0751cb0a0cab
Richard Ramos <info@richardramos.me>
2025-11-14 20:57:13 -0400

    fix: less agressive settings

--- lsquic/context/client.nim
+++ lsquic/context/client.nim
@@ -115,11 +115,18 @@ proc new*(
   lsquic_engine_init_settings(addr ctx.settings, 0)
   ctx.settings.es_versions = 1.cuint shl LSQVER_I001.cuint #IETF QUIC v1
   ctx.settings.es_cc_algo = BBRv1
-  ctx.settings.es_pace_packets = 1
-  ctx.settings.es_max_cfcw = 1
+  ctx.settings.es_max_cfcw = 32 * 1024 * 1024
   ctx.settings.es_dplpmtud = 1
   ctx.settings.es_base_plpmtu = 1280
-  ctx.settings.es_max_plpmtu = 4000
+  ctx.settings.es_max_plpmtu = 0
+  ctx.settings.es_pace_packets = 1
+
+  ctx.settings.es_cfcw = 4 * 1024 * 1024
+  ctx.settings.es_max_cfcw = 32 * 1024 * 1024
+  ctx.settings.es_sfcw = 1 * 1024 * 1024
+  ctx.settings.es_max_sfcw = 8 * 1024 * 1024
+  ctx.settings.es_max_batch_size = 64
+
   ctx.stream_if = struct_lsquic_stream_if(
     on_new_conn: onNewConn,
     on_hsk_done: onHandshakeDone,
