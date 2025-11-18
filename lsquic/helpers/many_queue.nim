3e72375b73b5d7547b418487f8147aa8bf80e9ab
vladopajic <vladopajic@users.noreply.github.com>
2025-11-18 14:51:14 +0100

    chore(many_queue): get() raises CancelledError (#17)

--- lsquic/helpers/many_queue.nim
+++ lsquic/helpers/many_queue.nim
@@ -7,8 +7,10 @@ type ManyQueue*[T] = ref object of RootRef
 proc new*[T](MQ: typedesc[ManyQueue[T]]): ManyQueue[T] =
   ManyQueue[T](data: @[], getFut: nil)
 
-proc get*[T](q: ManyQueue[T]): Future[seq[T]] =
-  let fut = Future[seq[T]].init("ManyQueue.get")
+proc get*[T](
+    q: ManyQueue[T]
+): Future[seq[T]] {.async: (raises: [CancelledError], raw: true).} =
+  let fut = newFuture[seq[T]]("ManyQueue.get")
 
   if q.data.len == 0:
     q.getFut = fut
@@ -22,5 +24,6 @@ proc put*[T](q: ManyQueue[T], e: sink T) =
   q.data.add(e)
 
   if not isNil(q.getFut):
-    q.getFut.complete(move q.data)
+    if not q.getFut.finished():
+      q.getFut.complete(move q.data)
     q.getFut = nil
