6f09a9f7d64c24510d68c2e4012e583eb927770a
Vlado Pajić <vlado@brrrrr.at>
2025-12-19 20:16:17 +0100

    nph

--- lsquic/context/context.nim
+++ lsquic/context/context.nim
@@ -1,7 +1,8 @@
 import chronos
 import chronos/osdefs
 import chronicles
-import ../[lsquic_ffi, errors, tlsconfig, timeout, certificates, certificateverifier, stream]
+import
+  ../[lsquic_ffi, errors, tlsconfig, timeout, certificates, certificateverifier, stream]
 
 let SSL_CTX_ID = SSL_CTX_get_ex_new_index(0, nil, nil, nil, nil) # Yes, this is global
 doAssert SSL_CTX_ID >= 0, "could not generate global ssl_ctx id"
--- lsquic/errors.nim
+++ lsquic/errors.nim
@@ -1,7 +1,7 @@
-type 
-    QuicError* = object of CatchableError
-    QuicConfigError* = object of QuicError
-    StreamError* = object of IOError
-    ConnectionError* = object of IOError
-    ConnectionClosedError* = object of ConnectionError
-    DialError* = object of IOError
+type
+  QuicError* = object of CatchableError
+  QuicConfigError* = object of QuicError
+  StreamError* = object of IOError
+  ConnectionError* = object of IOError
+  ConnectionClosedError* = object of ConnectionError
+  DialError* = object of IOError
