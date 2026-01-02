de410c4f0a29659f677f2ca5d7f4a8e812850082
vladopajic <vladopajic@users.noreply.github.com>
2026-01-02 19:05:14 +0100

    feat: add support for ipv6 (#39)

--- lsquic/helpers/transportaddr.nim
+++ lsquic/helpers/transportaddr.nim
@@ -11,16 +11,29 @@ import chronos
 import chronos/osdefs
 import ../lsquic_ffi
 
+# up until nim 2.2.6, AF_INET6 const had wrong value. 
+# that's why this const is defiend here to have backwards compatability with older versions of nim.
+# commit fix: https://github.com/nim-lang/Nim/commit/248850a0ce869c15fea16a35e248850d2df47c8d
+const fixed_AF_INET6 =
+  when defined(macosx):
+    30
+  elif defined(windows):
+    23
+  else:
+    10
+
+proc sockAddrLen*(family: int): SockLen {.inline.} =
+  case family
+  of AF_INET.int:
+    sizeof(Sockaddr_in).uint32
+  of fixed_AF_INET6.int: # use fixed const
+    sizeof(Sockaddr_in6).uint32
+  else:
+    raiseAssert "invalid socket address faimily"
+
 proc toTransportAddress*(sock: ptr SockAddr): TransportAddress =
   var destAddress: Sockaddr_storage
-  let destAddrLen: SockLen =
-    case sock.sa_family.uint16
-    of AF_INET.uint16:
-      sizeof(Sockaddr_in).uint32
-    of AF_INET6.uint16:
-      sizeof(Sockaddr_in6).uint32
-    else:
-      0.uint32
+  let destAddrLen: SockLen = sockAddrLen(sock.sa_family.int)
   copyMem(addr destAddress, sock, destAddrLen)
   var taddr: TransportAddress
   fromSAddr(addr destAddress, destAddrLen, taddr)
