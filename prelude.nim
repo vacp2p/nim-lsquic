ab41e864486068bae7b93c27eb163f4cfd94e018
Richard Ramos <info@richardramos.me>
2026-01-27 23:16:18 -0400

    fix: nimflags

--- prelude.nim
+++ prelude.nim
@@ -4,6 +4,9 @@
 when defined(windows):
   {.passc: "-D_WIN32_WINNT=0x0600".}
   {.passl: "-lws2_32".}
+  # ls-quic requires windows specific pthread otherwise the compiler will complain
+  # about missing pthread functions
+  {.passl: "-lwinpthread".}
 
 import std/[os, strformat, strutils]
 import chronos/osdefs
@@ -32,7 +35,11 @@ when defined(windows):
 {.passc: fmt"-I{xxhash}".}
 
 const HAVE_BORINGSSL = "-DHAVE_BORINGSSL"
-const XXH_HEADER_NAME = "-DXXH_HEADER_NAME='<lsquic_xxhash.h>'"
+const XXH_HEADER_NAME =
+  when defined(windows) and defined(clang):
+    "-DXXH_HEADER_NAME=<lsquic_xxhash.h>"
+  else:
+    "-DXXH_HEADER_NAME='<lsquic_xxhash.h>'"
 
 {.compile: "../libs/lsquic/src/liblsquic/lsquic_xxhash.c".}
 {.compile("../libs/lsquic/src/liblsquic/ls-qpack/lsqpack.c", XXH_HEADER_NAME).}
