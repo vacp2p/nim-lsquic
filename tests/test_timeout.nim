cb442ccb1d28fe4957273f230c617890307c4dd0
Radoslaw Kaminski <radoslaw@status.im>
2026-07-21 15:35:58 +0100

    use event instead of sleep

--- tests/test_timeout.nim
+++ tests/test_timeout.nim
@@ -6,34 +6,38 @@
 import chronos, chronos/unittest2/asynctests
 import lsquic/timeout
 
+const fireTimeout = 2.seconds
+
 suite "timeout":
   asyncTest "earlier deadline replaces later deadline":
-    var fired = 0
+    let fired = newAsyncEvent()
+    var fireCount = 0
     let timeout = newTimeout(
       proc() =
-        inc fired
+        inc fireCount
+        fired.fire()
     )
 
     timeout.set(400.milliseconds)
     timeout.set(50.milliseconds)
 
-    await sleepAsync(200.milliseconds)
-
-    check fired == 1
+    check (await fired.wait().withTimeout(fireTimeout))
+    check fireCount == 1
 
   asyncTest "later deadline does not replace earlier deadline":
-    var fired = 0
+    let fired = newAsyncEvent()
+    var fireCount = 0
     let timeout = newTimeout(
       proc() =
-        inc fired
+        inc fireCount
+        fired.fire()
     )
 
     timeout.set(50.milliseconds)
     timeout.set(400.milliseconds)
 
-    await sleepAsync(200.milliseconds)
-
-    check fired == 1
+    check (await fired.wait().withTimeout(fireTimeout))
+    check fireCount == 1
 
   asyncTest "stop cancels expiry":
     var fired = 0
